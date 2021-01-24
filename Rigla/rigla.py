3e3579a72161940e21eba2496f110d4a9b3bf79a
EmberGale <49767298+EmberGale@users.noreply.github.com>
2021-01-25 02:09:26 +0300

    oil

--- Rigla/rigla.py
+++ Rigla/rigla.py
@@ -10,8 +10,7 @@ def get_data(url=None):
         return False
 
     header = {'Content-Type': 'application/x-www-form-urlencoded',
-    'Accept': 'application/json, text/plain, */*',
-    'X-Requested-With': 'XMLHttpRequest'
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36'
     }
 
 #    wd = webdriver.Firefox()
@@ -20,46 +19,15 @@ def get_data(url=None):
 #    print(ids)
 
     s = requests.Session()
-    r = requests.get(url)
+    r = requests.get(url, headers=header)
 
     print("-GET-")
+    print(r.text)
     output = open("rigla.txt", 'w')
     output.write(r.text)
     output.close()
 
     return True
-
-def format_data (data):
-
-    geojson = {
-    "type": "FeatureCollection",
-    "features": [
-        {
-        "type": "Feature",
-        "properties" : {
-            "id": d["id"],
-            "name": d["name"],
-            "payments": [d["payments"]["card"], d["payments"]["terminal"]],
-            "email": d["email"],
-            "workTime": [d["workTime"]["fulltext"]],
-            "allowOrder": [d["allowOrder"]["status"], d["allowOrder"]["reason"]],
-            "hasRamp": d["hasRamp"],
-            "hasOptics": d["hasOptics"],
-            "hasOrthopedics": d["hasOrthopedics"],
-            "hasCosmetics": d["hasCosmetics"],
-            "hasSportFood": d["hasSportFood"],
-            "hasHomeopathy": d["hasHomeopathy"],
-            "hasNew": d["hasNew"],
-        },
-        "geometry" : {
-            "type": "Point",
-            "coordinates": [d['coordinates']['longitude'], d['coordinates']['latitude']],
-        },
-        } for value, d in data.items()]
-    }
-
-    return geojson
-
 
 def main ():
     data = get_data(url)
---
+++ m-oil/oil.py
@@ -0,0 +1,87 @@
+
+import requests
+import json
+from bs4 import BeautifulSoup
+import lxml
+import re
+
+url = "http://m-oil.ru/map/"
+
+def sort(data):
+    
+    geojson = {
+        "type": "FeatureCollection",
+        "features": [
+            {
+            "type": "Feature",
+            "properties" : {
+                'id': feature['id'],
+                    'name': feature['label'],
+                    'address': feature['address'],
+                    'se_tyres_restore': feature['se_tyres_restore'], 
+                    'se_extra_clean': feature['se_extra_clean'], 
+                    'se_vacuum': feature['se_vacuum'], 
+                    'se_coffee': feature['se_coffee'], 
+                    'se_terminal': feature['se_terminal'], 
+                    'se_gas': feature['se_gas'], 
+                    'se_three_d': feature['se_three_d'], 
+                    'se_coffe_baked': feature['se_coffe_baked'], 
+                    'fu_dt': feature['fu_dt'], 
+                    'fu_80': feature['fu_80'], 
+                    'fu_92': feature['fu_92'], 
+                    'fu_95': feature['fu_95'], 
+                    'fu_ai-100': feature['fu_ai-100'], 
+                    'fu_gaz': feature['fu_gaz'], 
+                    'pa_qiwi': feature['pa_qiwi'], 
+                    'pa_gazprom': feature['pa_gazprom'], 
+                    'pa_magistral': feature['pa_magistral'], 
+                    'pa_magnatek': feature['pa_magnatek'], 
+                    'pa_aris': feature['pa_aris'], 
+                    'pa_inforcom': feature['pa_inforcom'], 
+                    'pa_scanoil': feature['pa_scanoil'], 
+                    'pa_clubauto': feature['pa_clubauto'], 
+                    'pa_petrol': feature['pa_petrol'], 
+                    'pa_cards': feature['pa_cards'], 
+                    'pa_talon': feature['pa_talon'], 
+                    'pa_terminal-service': feature['pa_terminal-service'], 
+                    'pa_e100': feature['pa_e100'], 
+                    'pa_ballami-spasibo': feature['pa_ballami-spasibo'], 
+                    'pa_rosneft-card': feature['pa_rosneft-card']
+            },
+            "geometry" : {
+                "type": "Point",
+                "coordinates": [float(feature['longitude']), float(feature['latitude'])]
+            },
+            } for feature in data]
+        }
+
+    return geojson
+
+def get_data (url):
+    
+    response = requests.get(url) 
+    
+    soup = BeautifulSoup(response.text, 'lxml')
+
+    scripts = soup.findAll('script')
+    
+    for index, script in enumerate(scripts):
+        
+        script_content = script.string
+        if script_content is not None and "Map.Data.stations =" in script_content:
+            
+            match = re.search(r"Map.Data.stations = \[(.+)\]", str(script_content))
+            
+            data = json.loads(match.group(0).split('=')[1])
+
+            return data
+
+def main ():
+    data = get_data(url)
+    data = sort(data)
+    o = open("oil.geojson", 'w')
+    o.write(json.dumps(data))
+    o.close
+
+if __name__ == '__main__':
+    main()
